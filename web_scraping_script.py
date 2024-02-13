ce1bfa9fce7315b13d5a30a71c03aa62bc5b3da8
bajedo263 <73195062+bajedo263@users.noreply.github.com>
2024-02-13 22:06:59 +0100

    Update and rename python.py to web_scraping_script.py

---
+++ web_scraping_script.py
@@ -0,0 +1,8 @@
+# web_scraping_script.py
+# Utilisez des bibliothèques de scraping pour collecter les données depuis LinkedIn via Phantombuster
+
+# Code de scraping avec Phantombuster...
+# ...
+
+# Enregistrez les données dans un fichier CSV ou JSON
+# write_to_csv(data, 'linkedin_jobs_data.csv') ou write_to_json(data, 'linkedin_jobs_data.json')
