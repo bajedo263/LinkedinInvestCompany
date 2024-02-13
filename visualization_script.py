bf86e176d3c70fbc07fdce80e39dd164e47d5d6b
bajedo263 <73195062+bajedo263@users.noreply.github.com>
2024-02-13 22:08:52 +0100

    Update and rename total_investment_script.py to visualization_script.py

---
+++ visualization_script.py
@@ -0,0 +1,10 @@
+# visualization_script.py
+import matplotlib.pyplot as plt
+
+# Utilisez Matplotlib ou Seaborn pour créer des graphiques
+# Exemple simple avec Matplotlib
+plt.bar(total_investment.index, total_investment.values)
+plt.xlabel('Companies')
+plt.ylabel('Total Investment ($)')
+plt.title('Total Recruitment Investment by Company')
+plt.show()
